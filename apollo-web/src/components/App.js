2bc94f20a24f7e4c9ca49a759816ec4c9cfb7343
jamesbonglee <jamesbsec@gmail.com>
2021-04-12 21:18:41 +0900

    0.2 Router and Styles

--- apollo-web/src/components/App.js
+++ apollo-web/src/components/App.js
@@ -1,5 +1,14 @@
-function App() {
-   return <div className="App"></div>;
-}
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
+function App() {
+   return (
+      <Router>
+         <Route exact path="/" component={Home}></Route>
+         <Route path="/:id" component={Detail}></Route>
+      </Router>
+   );
+}
 export default App;
