1735e7502987f5fad581e7d5ca3aac881302dd2b
jamesbonglee <jamesbsec@gmail.com>
2021-04-14 05:11:09 +0900

    1.1 GET movies Query

--- apollo-web/src/routes/Home.js
+++ apollo-web/src/routes/Home.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+   {
+      movies {
+         id
+         medium_cover_image
+      }
+   }
+`;
+
+// 그래프ql가져와서 useQuery사용하여 데이터 가져오기 gql쿼리문은  GET_MOVIES 에 작성
+// map이용하여 id 가져오기
+export default () => {
+   const { loading, error, data } = useQuery(GET_MOVIES);
+   if (loading) {
+      return "loading...";
+   }
+   if (data && data.movies) {
+      return data.movies.map((m) => <h1>{m.id}</h1>);
+   }
+};
