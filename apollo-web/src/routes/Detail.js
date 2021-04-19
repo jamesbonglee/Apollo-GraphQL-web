0b5482b337d70ef9a880f9638cb5f1611a9c0538
jamesbonglee <jamesbonglee@gmail.com>
2021-04-19 23:58:33 +0900

    1.4 DATA get front

--- apollo-web/src/routes/Detail.js
+++ apollo-web/src/routes/Detail.js
@@ -1,6 +1,7 @@
+import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components";
 
 const GET_MOVIE = gql`
@@ -27,6 +28,7 @@ const Container = styled.div`
 
 const Column = styled.div`
    margin-left: 10px;
+   width: 50%;
 `;
 
 const Title = styled.h1`
@@ -47,23 +49,30 @@ const Poster = styled.div`
    width: 25%;
    height: 60%;
    background-color: transparent;
+   background-image: url(${(props) => props.bg});
+   background-size: cover;
+   background-position: center center;
 `;
 
 export default () => {
-   let { id } = useParams();
-   id = parseInt(id); // 스트링으로 받아와져서 gQL 에러문 발생하여 int로 변경
+   const { id } = useParams();
    const { loading, data } = useQuery(GET_MOVIE, {
       variables: { id },
    });
    return (
       <Container>
-         {" "}
          <Column>
-            <Title>Name</Title>
-            <Subtitle>English · 4.5</Subtitle>
-            <Description>lorem ipsum lalalla </Description>
+            <Title>{loading ? "Loading..." : data.movie.title}</Title>
+            {!loading && data.movie && (
+               <>
+                  <Subtitle>
+                     {data.movie.language} · {data.movie.rating}
+                  </Subtitle>
+                  <Description>{data.movie.description_intro}</Description>
+               </>
+            )}
          </Column>
-         <Poster></Poster>
+         <Poster bg={data && data.movie ? data.movie.medium_cover_image : ""}></Poster>
       </Container>
    );
 };
