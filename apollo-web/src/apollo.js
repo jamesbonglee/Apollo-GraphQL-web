b3d40121593187cb83827f6e55ca39573dbd10e8
jamesbonglee <jamesbonglee@gmail.com>
2021-04-22 05:04:30 +0900

    2.1 Local State part Two

--- apollo-web/src/apollo.js
+++ apollo-web/src/apollo.js
@@ -1,10 +1,21 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const client = new ApolloClient({
    uri: "//localhost:4000/",
+   cache: new InMemoryCache(),
    resolvers: {
       Movie: {
          isLiked: () => false,
+      },
+      Mutation: {
+         likeMovie: (_, { id }, { cache }) => {
+            cache.modify({
+               id: `Movie:${id}`,
+               fields: {
+                  isLiked: () => true,
+               },
+            });
+         },
       },
    },
 });
--- apollo-web/src/components/Movie.js
+++ apollo-web/src/components/Movie.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
 // HOme.js에서 movie 컴포넌트로 id를 map메서드 사용하여값을 넘겨주었음
 // 리액트에선 a태그를 사용하지않음 그래서 LINK사용
+
+const LIKE_MOVIE = gql`
+   mutation likeMovie($id: Int!) {
+      likeMovie(id: $id) @client
+   }
+`;
 
 const Container = styled.div`
    height: 400px;
@@ -21,11 +28,16 @@ const Poster = styled.div`
    border-radius: 7px;
 `;
 
-export default ({ id, bg, isLiked }) => (
-   <Container>
-      <Link to={`/${id}`}>
-         <Poster bg={bg} />
-      </Link>
-      <button>{isLiked ? "Unlike" : "Like"}</button>
-   </Container>
-);
+export default ({ id, bg, isLiked }) => {
+   const [likeMovie] = useMutation(LIKE_MOVIE, {
+      variables: { id: parseInt(id) },
+   });
+   return (
+      <Container>
+         <Link to={`/${id}`}>
+            <Poster bg={bg} />
+         </Link>
+         <button onClick={isLiked ? null : likeMovie}>{isLiked ? "Unlike" : "Like"}</button>
+      </Container>
+   );
+};
